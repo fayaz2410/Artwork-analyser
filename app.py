e3c436fb942ff722ae9e7f2ce67b9579b5d9b13d
SHAIK FAYAZ <fayaz93464@gmail.com>
2024-08-23 11:32:46 +0530

    changes done

--- app.py
+++ app.py
@@ -5,13 +5,12 @@ import io  # Import io to handle in-memory byte streams
 import json  # Import json to parse JSON responses
 import os # Import the os module for interacting with the operating system
 
-
 app = Flask(__name__)  # Initialize the Flask application
 
 # Define the Hugging Face API keys and endpoints
 captioning_api_url = "https://api-inference.huggingface.co/models/Salesforce/blip-image-captioning-large"
 sentiment_api_url = "https://api-inference.huggingface.co/models/cardiffnlp/twitter-roberta-base-sentiment"
-headers = {"Authorization": "Bearer YOUR_HUGGING_FACE_API_KEY"}  # Replace with your actual Hugging Face API key
+headers = {"Authorization": "Bearer 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"}  # Replace with your actual Hugging Face API key
 
 def generate_caption(image_url):
     """
@@ -91,4 +90,4 @@ def index():
     return render_template("index.html", description=description, emotion=emotion, image_url=image_url)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)  # Run the Flask application
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True) # Run the Flask application in debug mode
